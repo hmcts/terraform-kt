f780785b803c75e13050adc2801e07d240d3eb79
Daniel Wilson <daniel.wilson@kainos.com>
2024-09-13 11:54:31 +0100

    build rhel7 & ubu2004, test vm-bootstrap XDR install

--- components/vm/linux.tf
+++ components/vm/linux.tf
@@ -1,12 +1,20 @@
+
+data "azurerm_storage_account" "xdr_storage" {
+  provider            = azurerm.DTS-CFTPTL-INTSVC
+  name                = "cftptlintsvc"
+  resource_group_name = "core-infra-intsvc-rg"
+}
+
+# RHEL6
 module "vm_windows" {
   providers = {
     azurerm     = azurerm
     azurerm.cnp = azurerm.cnp
     azurerm.soc = azurerm.soc
   }
-  source               = "github.com/hmcts/terraform-module-virtual-machine.git?ref=cpareek-patch-1"
+  source               = "github.com/hmcts/terraform-module-virtual-machine.git?ref=bugfix/xdr-run-command-rhel6-azcopy"
   vm_type              = local.linux
-  vm_name              = "lin-test-vm"
+  vm_name              = "rhel6-test-vm"
   vm_resource_group    = azurerm_resource_group.rg.name
   vm_location          = azurerm_resource_group.rg.location
   vm_size              = "Standard_D4ds_v5"
@@ -18,9 +26,9 @@ module "vm_windows" {
   vm_private_ip        = "10.7.38.10"
   vm_public_ip_address = azurerm_public_ip.pubipt.id
   #storage_image_reference
-  vm_publisher_name          = "canonical"
-  vm_offer                   = "0001-com-ubuntu-server-jammy"
-  vm_sku                     = "22_04-lts-gen2"
+  vm_publisher_name          = "redhat-limited"
+  vm_offer                   = "rh-rhel"
+  vm_sku                     = "rh-rhel7"
   vm_version                 = "latest"
   install_dynatrace_oneagent = false
   install_splunk_uf          = false
@@ -46,8 +54,52 @@ resource "azurerm_public_ip" "pubipt" {
   tags                = merge(module.ctags.common_tags, { expiresAfter = local.expiresAfter })
 }
 
-data "azurerm_storage_account" "xdr_storage" {
-  provider            = azurerm.DTS-CFTPTL-INTSVC
-  name                = "cftptlintsvc"
-  resource_group_name = "core-infra-intsvc-rg"
+
+# Ubuntu 20.04
+module "vm_windows" {
+  providers = {
+    azurerm     = azurerm
+    azurerm.cnp = azurerm.cnp
+    azurerm.soc = azurerm.soc
+  }
+  source               = "github.com/hmcts/terraform-module-virtual-machine.git?ref=bugfix/xdr-run-command-rhel6-azcopy"
+  vm_type              = local.linux
+  vm_name              = "ubu2004-test-vm"
+  vm_resource_group    = azurerm_resource_group.rg.name
+  vm_location          = azurerm_resource_group.rg.location
+  vm_size              = "Standard_D4ds_v5"
+  vm_admin_password    = local.win_password
+  vm_availabilty_zones = "1"
+  nic_name             = "nic-linvm2"
+  ipconfig_name        = "IP_CONFIGURATION"
+  vm_subnet_id         = azurerm_subnet.subnet.id
+  vm_private_ip        = "10.7.38.20"
+  vm_public_ip_address = azurerm_public_ip.pubipt2.id
+  #storage_image_reference
+  vm_publisher_name          = "canonical"
+  vm_offer                   = "0001-com-ubuntu-server-jammy"
+  vm_sku                     = "22_04-lts-gen2"
+  vm_version                 = "latest"
+  install_dynatrace_oneagent = false
+  install_splunk_uf          = false
+  nessus_install             = false
+  env                        = "sbox"
+  tags                       = merge(module.ctags.common_tags, { expiresAfter = local.expiresAfter })
+
+
+  run_command        = true
+  run_command_sa_key = data.azurerm_storage_account.xdr_storage.primary_access_key
+  run_xdr_collector  = true
+  run_xdr_agent      = true
+  rc_script_file     = ""
+}
+
+resource "azurerm_public_ip" "pubipt2" {
+  name                = "linpub_ip2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  allocation_method   = "Static"
+  zones               = ["1"]
+  tags                = merge(module.ctags.common_tags, { expiresAfter = local.expiresAfter })
 }
