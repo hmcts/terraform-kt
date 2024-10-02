a895d429a4fb80c8493f37b927dac4fb36132d83
Daniel Wilson <daniel.wilson@kainos.com>
2024-10-02 16:58:55 +0100

    remove config alias from provider

--- components/vm/provider.tf
+++ components/vm/provider.tf
@@ -7,9 +7,8 @@ terraform {
 
   required_providers {
     azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = "4.2.0"
-      configuration_aliases = [azurerm.dcr]
+      source  = "hashicorp/azurerm"
+      version = "4.2.0"
     }
   }
 }
