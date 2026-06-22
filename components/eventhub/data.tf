a2ae52fb258001790e7b688942be9e1cb0e0d769
Salim Abdulkareem <salim.abdulkareem@justice.gov.uk>
2026-06-22 13:01:37 +0100

    fix

--- components/eventhub/data.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "ks_rg" {
-  name     = var.resource_group_name
-  location = "uksouth"
-
-  tags = merge(module.ctags.common_tags, { expiresAfter = local.expiresAfter })
-}
